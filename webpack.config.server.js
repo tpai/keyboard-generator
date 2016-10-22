865834e0f42e119f61406a1b41c3a39fab1161ab
tonypai <tony77794@gmail.com>
2016-10-22 16:51:32 +0800

    fix(webpack) replace nodeExternals with module

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -1,18 +1,11 @@
 var webpack = require("webpack");
 var path = require('path');
 var fs = require('fs');
-var nodeModules = {};
-
-// note the path.resolve(__dirname, ...) part
-// without it, eslint-import-resolver-webpack fails
-// since eslint might be invoked with different cwd
-fs.readdirSync(path.resolve(__dirname, 'node_modules'))
-    .filter(x => ['.bin'].indexOf(x) === -1)
-    .forEach(mod => { nodeModules[mod] = `commonjs ${mod}`; });
+var nodeExternals = require('webpack-node-externals');
 
 module.exports = {
     target: "node",
-    externals: nodeModules,
+    externals: [nodeExternals()],
     node: {
       __dirname: true
     },
